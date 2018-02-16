6d2b0fdc781af2963cc3ba90d9e19777451a36bf
fischphi <fischphi@student.ethz.ch>
2018-02-16 15:23:33 +0100

    Fixed save address bug

--- ReusabilitokenCustomerApp/app/src/main/java/token/reusabili/reusabilitokencustomer/MainActivity.java
+++ ReusabilitokenCustomerApp/app/src/main/java/token/reusabili/reusabilitokencustomer/MainActivity.java
@@ -13,7 +13,6 @@ import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 
@@ -29,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int BARCODE_READER_REQUEST_CODE = 1;
     private static final String FILENAME = "ReusabiliStore";
     private static final String VAL_KEY = "ADDRESS";
+    private SharedPreferences sharedPrefs;
     private EditText editAddress;
     public String Address = null;
     public static AHumanConfirmableAction choosenAction;
@@ -39,65 +39,59 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sharedPrefs  = getSharedPreferences(FILENAME, 0);
+        Address = sharedPrefs.getString(VAL_KEY, null);
         loadActivity();
 
-}
+    }
 
- private void loadActivity()
- {
-     final SharedPreferences sharedPrefs = getSharedPreferences(FILENAME, 0);
-     Address = sharedPrefs.getString(VAL_KEY, null);
-     if (!checkAddress(Address)) {
-         setContentView(R.layout.activity_main);
-         Toast.makeText(getApplicationContext(),"No correct ETH - Address set!", Toast.LENGTH_SHORT).show();
-         editAddress = (EditText) findViewById(R.id.enter_Address);
-
-         Button scanButton = (Button) findViewById(R.id.scan_barcode_button);
-         scanButton.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
-                 startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
-             }
-         });
-
-
-        try{
-         Button saveAddress = findViewById(R.id.save_address);
-         saveAddress.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-                 InputMethodManager inputManager = (InputMethodManager)
-                         getSystemService(Context.INPUT_METHOD_SERVICE);
-
-                 inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
-                         InputMethodManager.HIDE_NOT_ALWAYS);
-
-                 SharedPreferences.Editor editor = sharedPrefs.edit();
-                 String notNormalizedAddress = editAddress.getText().toString();
-                 if(notNormalizedAddress.length()==51) {
-                     Address=notNormalizedAddress.substring(9);
-                 }
-                 else
-                     Address=notNormalizedAddress;
-                 editor.putString(VAL_KEY, Address);
-                 editor.commit();
-                 Address=null;
-                 loadActivity();
-
-             }
-         });
-        }
-         catch(Exception e)
-         {
+    private void loadActivity()
+    {
+        if (!checkAddress(Address)) {
             Toast.makeText(getApplicationContext(),"No correct ETH - Address set!", Toast.LENGTH_SHORT).show();
-         }
-     }
+            editAddress = (EditText) findViewById(R.id.enter_Address);
+            Button scanButton = (Button) findViewById(R.id.scan_barcode_button);
+            scanButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                    startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
+                }
+            });
 
-     else
-     {
-         startActivity(new Intent(this, ClaimActivity.class));
-     }
- }
+
+
+            Button saveAddress = findViewById(R.id.save_address);
+            saveAddress.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    InputMethodManager inputManager = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                    inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+                    String notNormalizedAddress = editAddress.getText().toString();
+                    if(checkAddress(notNormalizedAddress)) {
+                        SharedPreferences.Editor editor = sharedPrefs.edit();
+                        if (notNormalizedAddress.length() == 51) {
+                            Address = notNormalizedAddress.substring(9);
+                        } else
+                            Address = notNormalizedAddress;
+                        editor.putString(VAL_KEY, Address);
+                        editor.commit();
+                    }
+                    loadActivity();
+
+                }
+            });
+
+        }
+
+        else
+        {
+            startActivity(new Intent(this, ClaimActivity.class));
+        }
+    }
 
     private boolean checkAddress(String address)
     {
@@ -106,22 +100,23 @@ public class MainActivity extends AppCompatActivity {
         String eth="ethereum:";
         String hexsig, number;
         boolean length = address.length()==42 || address.length()== 51;
-        if(!(address.length()==51)) {
+        if(!(address.length()==51) && address.length()>2) {
             hexsig = address.substring(0, 2);
             number = address.substring(2);
         }
-        else{
+        else if((address.length()==51)){
             eth = address.substring(0,9);
             hexsig = address.substring(9, 11);
             number = address.substring(11);
         }
+        else {hexsig=""; number="";}
         boolean ethsig = eth.toLowerCase().contains(new String("ethereum:"));
         boolean ishex = hexsig.toLowerCase().contains(new String("0x"));
         boolean asciichar = true;
         for (char character : number.toCharArray()) {
-                int code = (int) character;
-                if(!((code >= 48 && code <= 57) || (code >= 65 && code <= 70) || (code >= 97 && code <= 102)))
-                    asciichar = false;
+            int code = (int) character;
+            if(!((code >= 48 && code <= 57) || (code >= 65 && code <= 70) || (code >= 97 && code <= 102)))
+                asciichar = false;
         }
 
 
@@ -143,4 +138,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
